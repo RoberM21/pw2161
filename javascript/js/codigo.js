4a18141a1bc2577a304102525b9b1847086525de
roberm21 <roberm21@outlook.com>
2016-04-12 10:59:39 -0600

    Archivo js, funcion anonima y consola

---
+++ javascript/js/codigo.js
@@ -0,0 +1,10 @@
+	
+	var botonClic = function()
+	{
+		alert("clic del botón");
+	}
+
+	var nombre = "Roberto";
+	document.write("<strong>JS en accion</strong>"+" "+nombre);
+	console.log("Apunto de lanzar un Mensaje");
+	alert("Mensaje por pantalla");
