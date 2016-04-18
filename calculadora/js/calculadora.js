14eb67bca989f5ad1d00f95b70c9ca3fd97e44d3
roberm21 <roberm21@outlook.com>
2016-04-18 10:32:29 -0600

    Funcion borrar de calculadora terminada

--- calculadora/js/calculadora.js
+++ calculadora/js/calculadora.js
@@ -10,29 +10,37 @@ function igual()
 
 function numeros(num) 
 {
-	
+	var valor = document.calculadora.operando1.value;
 	if(operador=="") //Escribir en el operador
 	{
-		var valor = document.calculadora.operando1.value;
 		if(valor=="0")//vaciamos la caja;
 		{
 			document.calculadora.operando1.value="";
 		}
 			//concatenar los valores de num con los de operador1
-			document.calculadora.operando1.value = document.calculadora.operador1.value+num;
+			document.calculadora.operando1.value = document.calculadora.operador1.value + num;
+	}
 		else
 		{
 			var valor = document.calculadora.operando2.value;
 			if(valor=="0")//vaciamos la caja;
 			{
-			document.calculadora.operando2.value="";
+				document.calculadora.operando2.value="";
 			}
 			//concatenar los valores de num con los de operador1
-			document.calculadora.operando2.value = document.calculadora.operador2.value+num;
+			document.calculadora.operando2.value = document.calculadora.operador2.value + num;
 		}
-	}
+	
 }
 function operadores(ope)
 {
 	operador=ope;
+}
+
+function borrar()
+{
+	operador="";
+	document.calculadora.operador1.value=0;
+	document.calculadora.operando2.value=0;
+	document.calculadora.resultado.value=0;
 }
