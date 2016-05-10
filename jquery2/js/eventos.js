379d1c8e58334c1035748228b4b4fa43a32b6ca1
roberm21 <roberm21@outlook.com>
2016-05-10 11:07:13 -0600

    Sistema de ejemplos con usuario y contraseña, falta validar

--- jquery2/js/eventos.js
+++ jquery2/js/eventos.js
@@ -1,1 +1,23 @@
-eventos.js
+var iniciaApp = function()
+{
+	var validarEntrada = function()
+	{
+		var usuario = $("#txtUsuario").val();
+		var contraseña = $("#txtContraseña").val();
+		//validaciones
+		//1.- QUE NO SEAN VACIOS
+		if(usuario == "")
+		{
+			alert("El usuario no debe ser vacio");
+			$("#txtUsuario").focus();
+		}
+		if(contraseña == "")
+		{
+			alert("La clave no debe ser vacio");
+			$("#txtContraseña").focus();
+		}
+		console.log("se disparo el submit");
+	}
+	$("#frmValidaEntrada").on("submit",validarEntrada);
+}
+$(document).on("ready",iniciaApp);
